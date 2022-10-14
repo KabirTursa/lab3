20ddaa2e65f21d4e61910510d85e1e0319eaf944
KabirTursa <105084172+KabirTursa@users.noreply.github.com>
2022-10-14 14:50:13 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CharList a = new CharList("Batman");
+        System.out.println(a.toString());
+    }
+}
