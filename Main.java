b07dd23b5e5e9396510e4916eb0c6ab5501c42da
KabirTursa <105084172+KabirTursa@users.noreply.github.com>
2022-10-18 17:36:01 -0700

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-
-        CharList a = new CharList("Batman");
-        System.out.println(a.toString());
-    }
-}
